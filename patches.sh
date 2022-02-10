a278973c672f595549e846a937888db9d9407af4
jhenrique09 <jhsv09@gmail.com>
2022-02-09 22:01:24 -0300

    ParanoidSense: Add back stock deps
    Change-Id: I03896518543c80de5610f8d905de7057aa7a8217

---
+++ patches.sh
@@ -0,0 +1,2 @@
+sed -i "s/com.motorola.faceunlock.util.MotoUnlockEncryptor/faceunlock\/util\/FaceUnlockEncryptorDependencyLib/" lib/libarcsoft-lib.so
+sed -i "s/android.os.Build/faceunlock\/Build/" lib/libarcsoft-lib.so
